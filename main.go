f23b1d6503fe42b5b7365c4955d4cc1df1349b57
thecyberbear <nguyenthdat@outlook.com>
2021-11-10 06:29:30 +0000

    update code

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -15,4 +19,23 @@ func main() {
 	defer file.Close()
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("tuoitre.vn"),
+	)
+	c.OnHTML(".name-news", func(e *colly.HTMLElement) {
+		writer.Write([]string{
+			e.ChildText("a"),
+			e.ChildText("p"),
+			e.ChildText("span"),
+		})
+	})
+
+	for i := 0; i < 100; i++ {
+		fmt.Printf("Scraping page: %d\n", i)
+		c.Visit("https://tuoitre.vn/kinh-doanh/doanh-nghiep/trang-" + strconv.Itoa(i) + ".htm")
+	}
+
+	log.Printf("Scraping Done\n")
+	log.Println(c)
 }
